2a1f3d78c1f27a2a4edc29a921b98e5098ff7ef2
Guillaume Delhumeau <guillaume.delhumeau@xwiki.com>
2015-01-29 16:03:42 +0100

    XCS-17: Implement the ability to select a flavor when we create a new wiki.
    * Send the good events while provisioning wikis.

--- xcs-applications/xcs-wikiflavor/xcs-wikiflavor-api/src/main/java/com/xwikisas/xcs/wikiflavor/internal/steps/ProvisionWikiStep.java
+++ xcs-applications/xcs-wikiflavor/xcs-wikiflavor-api/src/main/java/com/xwikisas/xcs/wikiflavor/internal/steps/ProvisionWikiStep.java
@@ -21,13 +21,19 @@ package com.xwikisas.xcs.wikiflavor.internal.steps;
 
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.inject.Provider;
 
+import org.xwiki.bridge.event.WikiCopiedEvent;
+import org.xwiki.bridge.event.WikiProvisioningEvent;
+import org.xwiki.bridge.event.WikiProvisioningFailedEvent;
 import org.xwiki.component.annotation.Component;
 import org.xwiki.component.annotation.InstantiationStrategy;
 import org.xwiki.component.descriptor.ComponentInstantiationStrategy;
-import org.xwiki.wiki.manager.WikiManagerException;
+import org.xwiki.observation.ObservationManager;
 import org.xwiki.wiki.internal.manager.WikiCopier;
+import org.xwiki.wiki.manager.WikiManagerException;
 
+import com.xpn.xwiki.XWikiContext;
 import com.xwikisas.xcs.wikiflavor.WikiCreationRequest;
 import com.xwikisas.xcs.wikiflavor.WikiCreationStep;
 import com.xwikisas.xcs.wikiflavor.WikiFlavorException;
@@ -50,24 +56,54 @@ public class ProvisionWikiStep implements WikiCreationStep
     @Inject
     private ExtensionInstaller extensionInstaller;
 
+    @Inject
+    private ObservationManager observationManager;
+
+    @Inject
+    private Provider<XWikiContext> xcontextProvider;
+
     @Override
     public void execute(WikiCreationRequest request) throws WikiFlavorException
     {
         try {
             switch (request.getWikiSource()) {
                 case EXTENSION:
+                    sendWikiProvisioningEvent(request);
+
                     extensionInstaller.installExtension(request.getWikiId(), request.getExtensionId());
+
+                    sendWikiProvisionedEvent(request);
                     break;
                 case TEMPLATE:
+                    sendWikiProvisioningEvent(request);
+
                     wikiCopier.copyDocuments(request.getTemplateId(), request.getWikiId(), false);
+                    observationManager.notify(new WikiCopiedEvent(request.getTemplateId(), request.getWikiId()),
+                        request.getTemplateId(), xcontextProvider.get());
+
+                    sendWikiProvisionedEvent(request);
                     break;
                 default:
                     // No source is defined, we let the wiki empty and DW will do the rest
                     break;
             }
         } catch (WikiManagerException|WikiFlavorException e) {
+            observationManager.notify(new WikiProvisioningFailedEvent(request.getWikiId()), request.getWikiId(),
+                xcontextProvider.get());
             throw new WikiFlavorException(String.format("Failed to provision the wiki [%s]", request.getWikiId()), e);
         }
+    }
+
+    private void sendWikiProvisioningEvent(WikiCreationRequest request)
+    {
+        observationManager.notify(new WikiProvisioningEvent(request.getWikiId()), request.getWikiId(),
+            xcontextProvider.get());
+    }
+
+    private void sendWikiProvisionedEvent(WikiCreationRequest request)
+    {
+        observationManager.notify(new WikiProvisioningEvent(request.getWikiId()), request.getWikiId(),
+            xcontextProvider.get());
     }
 
     @Override
