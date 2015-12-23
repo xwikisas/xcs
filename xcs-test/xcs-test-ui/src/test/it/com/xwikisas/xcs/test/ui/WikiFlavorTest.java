6b68b7529bbe2ffb4a4bf6bf16384499232fb8fb
Guillaume Delhumeau <guillaume.delhumeau@xwiki.com>
2015-12-23 15:34:44 +0100

    XCS-178: Upgrade Wiki Flavor Application to 2.1.
    * Fix tests.

--- xcs-test/xcs-test-ui/src/test/it/com/xwikisas/xcs/test/ui/WikiFlavorTest.java
+++ xcs-test/xcs-test-ui/src/test/it/com/xwikisas/xcs/test/ui/WikiFlavorTest.java
@@ -24,14 +24,13 @@ import java.util.List;
 import org.contrib.wikiflavor.tests.po.CreateFlavoredWikiPage;
 import org.contrib.wikiflavor.tests.po.Flavor;
 import org.contrib.wikiflavor.tests.po.Template;
-import org.contrib.wikiflavor.tests.po.WikiCreationProvisioningPage;
 import org.junit.Rule;
 import org.junit.Test;
 import org.xwiki.test.ui.AbstractTest;
 import org.xwiki.test.ui.AdminAuthenticationRule;
 import org.xwiki.test.ui.po.editor.WikiEditPage;
-import org.xwiki.wiki.test.po.CreateWikiPageStepProvisioning;
 import org.xwiki.wiki.test.po.CreateWikiPageStepUser;
+import org.xwiki.wiki.test.po.WikiCreationPage;
 import org.xwiki.wiki.test.po.WikiHomePage;
 import org.xwiki.wiki.test.po.WikiIndexPage;
 import org.xwiki.wiki.test.po.WikiLink;
@@ -47,8 +46,7 @@ import static org.junit.Assert.assertTrue;
 public class WikiFlavorTest extends AbstractTest
 {
     @Rule
-    public AdminAuthenticationRule adminAuthenticationRule =
-            new AdminAuthenticationRule(getUtil(), getDriver());
+    public AdminAuthenticationRule adminAuthenticationRule = new AdminAuthenticationRule(getUtil());
 
     @Test
     public void verifyInstalledFlavors() throws Exception
@@ -62,20 +60,26 @@ public class WikiFlavorTest extends AbstractTest
         List<Flavor> flavors = createFlavoredWikiPage.getFlavors();
         assertEquals(3, flavors.size());
 
+        // TODO: set the version by using the maven version
+        String extensionVersion = flavors.get(0).getExtensionVersion();
+
         // Verify that the flavor is the one we just have created
         // We expect the 3 following flavors:
         Flavor standardWiki = new Flavor();
         standardWiki.setName("Standard Wiki");
         standardWiki.setDescription("Basic wiki with default applications installed");
         standardWiki.setExtensionId("com.xwikisas:xcs-ui-wiki");
+        standardWiki.setExtensionVersion(extensionVersion);
         Flavor knowledgeBase = new Flavor();
         knowledgeBase.setName("Knowledge Base");
         knowledgeBase.setDescription("Allows you to collect, organize and share information about one or several topics");
         knowledgeBase.setExtensionId("com.xwikisas:xcs-ui-knowledgebase");
+        knowledgeBase.setExtensionVersion(extensionVersion);
         Flavor workspace = new Flavor();
         workspace.setName("Workspace");
         workspace.setDescription("Features collaborative applications allowing you to collaborate on a project");
         workspace.setExtensionId("com.xwikisas:xcs-ui-workspace");
+        workspace.setExtensionVersion(extensionVersion);
         assertTrue(flavors.contains(standardWiki));
         assertTrue(flavors.contains(knowledgeBase));
         assertTrue(flavors.contains(workspace));
@@ -90,6 +94,7 @@ public class WikiFlavorTest extends AbstractTest
         CreateFlavoredWikiPage createFlavoredWikiPage = new CreateFlavoredWikiPage();
 
         // Get the list of templates
+        createFlavoredWikiPage.selectTemplateTab();
         List<Template> templates = createFlavoredWikiPage.getTemplates();
         assertEquals(0, templates.size());
     }
@@ -106,24 +111,24 @@ public class WikiFlavorTest extends AbstractTest
         createFlavoredWikiPage.setDescription("My subwiki which gonna be a template.");
 
         // Set the type of wiki we want
-        createFlavoredWikiPage.setFlavorOrExtension("Standard Wiki");
+        createFlavoredWikiPage.setFlavor("Standard Wiki");
         // This wiki will be a template
+        createFlavoredWikiPage.selectTemplateTab();
         createFlavoredWikiPage.setIsTemplate(true);
 
         // Step 2
         CreateWikiPageStepUser createWikiPageStepUser = createFlavoredWikiPage.goUserStep();
-        createWikiPageStepUser.createWithTemplate();
-        WikiCreationProvisioningPage wikiCreationProvisioningPage = new WikiCreationProvisioningPage();
+        WikiCreationPage wikiCreationPage = createWikiPageStepUser.create();
 
         // Provisioning
-        assertEquals("Wiki creation", wikiCreationProvisioningPage.getStepTitle());
+        assertEquals("Wiki creation", wikiCreationPage.getStepTitle());
         
         // The installation is quite long
-        wikiCreationProvisioningPage.waitForFinalizeButton(60*3); // 3 minutes
-        assertFalse(wikiCreationProvisioningPage.hasLogError());
+        wikiCreationPage.waitForFinalizeButton(60*3); // 3 minutes
+        assertFalse(wikiCreationPage.hasLogError());
 
         // Finalization
-        WikiHomePage wikiHomePage = wikiCreationProvisioningPage.finalizeCreation();
+        WikiHomePage wikiHomePage = wikiCreationPage.finalizeCreation();
 
         // Go to the created subwiki
 
@@ -139,36 +144,36 @@ public class WikiFlavorTest extends AbstractTest
         createFlavoredWikiPage.setPrettyName("My other subwiki");
 
         // Get the list of templates
+        createFlavoredWikiPage.selectTemplateTab();
         List<Template> templates = createFlavoredWikiPage.getTemplates();
         assertEquals(1, templates.size());
 
         // Verify that the template is the one we just have created
         Template myTemplate = templates.get(0);
         assertEquals("My subwiki (mysubwiki)", myTemplate.getName());
-        assertEquals("My subwiki which gonna be a template.", myTemplate.getDescription());
         assertEquals("mysubwiki", myTemplate.getTemplateId());
 
         // Set the type of wiki we want
-        createFlavoredWikiPage.setFlavorOrExtension("My subwiki (mysubwiki)");
+        createFlavoredWikiPage.setTemplate("My subwiki (mysubwiki)");
 
         // Step 2
         createWikiPageStepUser = createFlavoredWikiPage.goUserStep();
-        CreateWikiPageStepProvisioning createWikiPageStepProvisioning = createWikiPageStepUser.createWithTemplate();
+        wikiCreationPage = createWikiPageStepUser.create();
 
         // Provisioning
-        assertEquals("Wiki creation", createWikiPageStepProvisioning.getStepTitle());
-        wikiCreationProvisioningPage.waitForFinalizeButton(60 * 2);
-        assertFalse(wikiCreationProvisioningPage.hasLogError());
-        wikiHomePage = createWikiPageStepProvisioning.finalizeCreation();
+        assertEquals("Wiki creation", wikiCreationPage.getStepTitle());
+        wikiCreationPage.waitForFinalizeButton(60 * 2);
+        assertFalse(wikiCreationPage.hasLogError());
+        wikiHomePage = wikiCreationPage.finalizeCreation();
 
         // Go to the subwiki and check that it has correctly be created with the template
         assertEquals("My Template", wikiHomePage.getDocumentTitle());
 
         // Cleaning
-        wikiHomePage.deleteWiki().confirm();
+        wikiHomePage.deleteWiki().confirm("myothersubwiki");
         wikiIndexPage = WikiIndexPage.gotoPage();
         wikiHomePage = wikiIndexPage.getWikiLink("My subwiki").click();
-        wikiHomePage.deleteWiki().confirm();
+        wikiHomePage.deleteWiki().confirm("mysubwiki");
     }
 
     @Test
@@ -181,22 +186,21 @@ public class WikiFlavorTest extends AbstractTest
         createFlavoredWikiPage.setPrettyName("My Workspace");
 
         // Set the type of wiki we want
-        createFlavoredWikiPage.setFlavorOrExtension("Workspace");
+        createFlavoredWikiPage.setFlavor("Workspace");
         
         // Step 2
         CreateWikiPageStepUser createWikiPageStepUser = createFlavoredWikiPage.goUserStep();
-        createWikiPageStepUser.createWithTemplate();
-        WikiCreationProvisioningPage wikiCreationProvisioningPage = new WikiCreationProvisioningPage();
+        WikiCreationPage wikiCreationPage = createWikiPageStepUser.create();
 
         // Provisioning
-        assertEquals("Wiki creation", wikiCreationProvisioningPage.getStepTitle());
+        assertEquals("Wiki creation", wikiCreationPage.getStepTitle());
 
         // The installation is quite long
-        wikiCreationProvisioningPage.waitForFinalizeButton(60*3); // 3 minutes
-        assertFalse(wikiCreationProvisioningPage.hasLogError());
+        wikiCreationPage.waitForFinalizeButton(60*3); // 3 minutes
+        assertFalse(wikiCreationPage.hasLogError());
 
         // Finalization
-        wikiCreationProvisioningPage.finalizeCreation();
+        wikiCreationPage.finalizeCreation();
 
         // Go to the list of wikis
         wikiIndexPage = WikiIndexPage.gotoPage();
@@ -205,7 +209,7 @@ public class WikiFlavorTest extends AbstractTest
         assertEquals("My Workspace", wikiLink.getWikiName());
 
         // Cleaning
-        wikiLink.click().deleteWiki().confirm();
+        wikiLink.click().deleteWiki().confirm("myworkspace");
     }
 
     @Test
@@ -218,22 +222,21 @@ public class WikiFlavorTest extends AbstractTest
         createFlavoredWikiPage.setPrettyName("My Knowledge Base");
 
         // Set the type of wiki we want
-        createFlavoredWikiPage.setFlavorOrExtension("Knowledge Base");
+        createFlavoredWikiPage.setFlavor("Knowledge Base");
 
         // Step 2
         CreateWikiPageStepUser createWikiPageStepUser = createFlavoredWikiPage.goUserStep();
-        createWikiPageStepUser.createWithTemplate();
-        WikiCreationProvisioningPage wikiCreationProvisioningPage = new WikiCreationProvisioningPage();
+        WikiCreationPage wikiCreationPage = createWikiPageStepUser.create();
 
         // Provisioning
-        assertEquals("Wiki creation", wikiCreationProvisioningPage.getStepTitle());
+        assertEquals("Wiki creation", wikiCreationPage.getStepTitle());
 
         // The installation is quite long
-        wikiCreationProvisioningPage.waitForFinalizeButton(60*3); // 3 minutes
-        assertFalse(wikiCreationProvisioningPage.hasLogError());
+        wikiCreationPage.waitForFinalizeButton(60*3); // 3 minutes
+        assertFalse(wikiCreationPage.hasLogError());
 
         // Finalization
-        wikiCreationProvisioningPage.finalizeCreation();
+        wikiCreationPage.finalizeCreation();
 
         // Go to the list of wikis
         wikiIndexPage = WikiIndexPage.gotoPage();
@@ -242,6 +245,6 @@ public class WikiFlavorTest extends AbstractTest
         assertEquals("My Knowledge Base", wikiLink.getWikiName());
 
         // Cleaning
-        wikiLink.click().deleteWiki().confirm();
+        wikiLink.click().deleteWiki().confirm("myknowledgebase");
     }
 }
